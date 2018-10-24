6544f1c5bf65e1d54189e9fbfde8b01c6435ec07
Dima Onopko <onopko24@gmail.com>
2018-10-24 21:20:07 +0300

    pre finish 1200

--- src/js/common.js
+++ src/js/common.js
@@ -1,17 +1,15 @@
-document.addEventListener('click', function(e){
-    console.log(e.target);
-    e.preventDefault();
-    if (e.target.closest('.toggle__menu')) {
-        console.log('OK = ', e.target.closest('.toggle__menu'));
-        document.getElementsByClassName('toggle')
-        [0].classList.toggle('navbar__active')
-    }else{
-        console.log(e.target.closest('.toggle__menu'));
-    }
-})
+dropdown();
 
-function replaceElem(){
-    var remText = document.getElementById('removeText');
-    document.getElementById('pastText').appendChild(remText);
-};
-replaceElem();
+function dropdown(){
+    document.addEventListener('click', function(e){
+        console.log(e.target);
+        e.preventDefault();
+        if (e.target.closest('.toggle__menu')) {
+            console.log('OK = ', e.target.closest('.toggle__menu'));
+            document.getElementsByClassName('toggle')
+            [0].classList.toggle('navbar__active')
+        }else{
+            console.log(e.target.closest('.toggle__menu'));
+        }
+    })
+}
